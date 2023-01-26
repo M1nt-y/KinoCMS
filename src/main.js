c94ebf03b7ba7c0cfeabd47fb46f53134735aae0
M1nt-y <sviatoslav.ivashchenko@student.sumdu.edu.ua>
2023-01-26 16:10:03 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from '@/router/router'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
-createApp(App).mount('#app')
+
+const app = createApp(App);
+
+app.use(createPinia(piniaPluginPersistedstate)).use(router).mount('#app');
